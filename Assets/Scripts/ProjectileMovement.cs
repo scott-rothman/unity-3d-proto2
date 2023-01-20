47066937aa3f2795190b69e35f51288204fca5b8
Scott Rothman <scott.r.rothman@gmail.com>
2023-01-19 23:04:35 -0500

    Add collision detection
    Tagged "lethal" game objects, log lose statement when they move off bottom of screen
    
    Add collision for pizza and lethal game objects

--- Assets/Scripts/ProjectileMovement.cs
+++ Assets/Scripts/ProjectileMovement.cs
@@ -7,14 +7,22 @@ public class ProjectileMovement : MonoBehaviour
     public float speed = 40f;
     private float topPosBounds = 30f;
     private float bottomPosBounds = -10f;
+    private
 
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
-        if (transform.position.z > topPosBounds ||
-            transform.position.z < bottomPosBounds)
+        if (transform.position.z > topPosBounds)
         {
+            Destroy(gameObject);
+        }
+        else if (
+            transform.position.z < bottomPosBounds &&
+            gameObject.tag == "LethalProjectile"
+        ) 
+        {
+            Debug.Log("Game Over!!!");
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 20f;
+    private float spawnPositionZ = 20f;
+
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+
+    private void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
